c51a57b6755b76d0d01a2d713ff1466a57b4374d
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-09-01 19:09:55 -0300

    Add Kangaroo exercise.

---
+++ src/main/java/implementation/Kangaroo.java
@@ -0,0 +1,33 @@
+package implementation;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/kangaroo/problem
+
+class Kangaroo {
+
+    private static String kangaroo(int x1, int v1, int x2, int v2) {
+        if ((x1 < x2) && (v1 < v2))
+            return "NO";
+        else {
+            if ((v1 != v2) && ((x2 - x1) % (v1 - v2)) == 0) {
+                return "YES";
+            } else {
+                return "NO";
+            }
+        }
+    }
+
+    public void solve() {
+        // Given code
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+
+        String result = kangaroo(x1, v1, x2, v2);
+
+        System.out.println(result);
+    }
+}
---
+++ src/test/java/implementation/KangarooTest.java
@@ -0,0 +1,47 @@
+package implementation;
+
+import common.AbstractInOutTest;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KangarooTest extends AbstractInOutTest {
+    private Kangaroo exercise = new Kangaroo();
+
+    private static final String INPUT_CASE0 = "0 3 4 2\n";
+    private static final String OUTPUT_CASE0 = "YES\n";
+
+    private static final String INPUT_CASE1 = "0 2 5 3\n";
+    private static final String OUTPUT_CASE1 = "NO\n";
+
+    private static final String INPUT_CASE13 = "112 9563 8625 244\n";
+    private static final String OUTPUT_CASE13 = "NO\n";
+
+    @Test
+    public void shouldSolveExerciseForFirstTestInput() {
+        input(INPUT_CASE0);
+
+        exercise.solve();
+
+        assertThat(output()).isEqualTo(OUTPUT_CASE0);
+    }
+
+    @Test
+    public void shouldSolveExerciseForSecondTestInput() {
+        input(INPUT_CASE1);
+
+        exercise.solve();
+
+        assertThat(output()).isEqualTo(OUTPUT_CASE1);
+    }
+
+    @Test
+    public void shouldSolveExerciseForResult13() {
+        input(INPUT_CASE13);
+
+        exercise.solve();
+
+        assertThat(output()).isEqualTo(OUTPUT_CASE13);
+    }
+
+}
