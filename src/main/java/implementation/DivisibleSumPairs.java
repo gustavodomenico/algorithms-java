80f3c4116c03bda8979e9f0460f9876699deea2c
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-09-03 20:33:08 -0300

    Add Divisible Sum Pairs exercise.

---
+++ src/main/java/implementation/DivisibleSumPairs.java
@@ -0,0 +1,40 @@
+package implementation;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+
+class DivisibleSumPairs {
+
+    private static int divisibleSumPairs(int n, int k, int[] ar) {
+        int matches = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = ar[i];
+
+            for (int j = 0; j < n; j++) {
+                int b = ar[j];
+
+                if (i < j && (a + b) % k == 0) {
+                    matches++;
+                }
+            }
+        }
+
+        return matches;
+    }
+
+    public void solve() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int[] ar = new int[n];
+        for (int ar_i = 0; ar_i < n; ar_i++) {
+            ar[ar_i] = in.nextInt();
+        }
+
+        int result = divisibleSumPairs(n, k, ar);
+        System.out.println(result);
+    }
+}
