6bb396926719ee21cb9af09362059b48d373c57a
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-09-04 17:24:41 -0300

    Add Bon Appetit exercise.

---
+++ src/main/java/implementation/BonAppetit.java
@@ -0,0 +1,43 @@
+package implementation;
+
+// https://www.hackerrank.com/challenges/bon-appetit/problem
+
+import java.util.Scanner;
+
+import static java.lang.String.valueOf;
+
+class BonAppetit {
+
+    private static String bonAppetit(int n, int k, int b, int[] ar) {
+        int fairAmount = 0;
+        for (int i = 0; i < n ; i++) {
+            if (i != k) {
+                fairAmount+= ar[i];
+            }
+        }
+
+        if (b == fairAmount / 2) {
+            return "Bon Appetit";
+        }
+
+        return  valueOf(b - (fairAmount / 2));
+    }
+
+    public void solve() {
+        // Given code
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+
+        int b = in.nextInt();
+
+        String result = bonAppetit(n, k, b, ar);
+        System.out.println(result);
+    }
+}
---
+++ src/test/java/implementation/BonAppetitTest.java
@@ -0,0 +1,41 @@
+package implementation;
+
+import common.AbstractInOutTest;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BonAppetitTest extends AbstractInOutTest {
+    private BonAppetit exercise = new BonAppetit();
+
+    private static final String INPUT_SAMPLE0 = "4 1\n" +
+            "3 10 2 9\n" +
+            "12\n";
+
+    private static final String OUTPUT_SAMPLE0 = "5\n";
+
+    private static final String INPUT_SAMPLE1 = "4 1\n" +
+            "3 10 2 9\n" +
+            "7\n";
+
+    private static final String OUTPUT_SAMPLE1 = "Bon Appetit\n";
+
+    @Test
+    public void shouldSolveExerciseForFirstTestInput() {
+        input(INPUT_SAMPLE0);
+
+        exercise.solve();
+
+        assertThat(output()).isEqualTo(OUTPUT_SAMPLE0);
+    }
+
+    @Test
+    public void shouldSolveExerciseForSecondTestInput() {
+        input(INPUT_SAMPLE1);
+
+        exercise.solve();
+
+        assertThat(output()).isEqualTo(OUTPUT_SAMPLE1);
+    }
+
+}
