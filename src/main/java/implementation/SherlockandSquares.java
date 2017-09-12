9e67c0330832af64b1ea38cb391e74e7d716fd33
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-09-12 00:06:29 -0300

    Add Shelock And Squares exercise.

---
+++ src/main/java/implementation/SherlockandSquares.java
@@ -0,0 +1,47 @@
+package implementation;
+
+import common.Exercise;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.ceil;
+import static java.lang.Math.floor;
+import static java.lang.Math.sqrt;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+// https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+
+class SherlockandSquares implements Exercise {
+
+    public void solve() {
+        Scanner scanner = new Scanner(in);
+
+        int cases = parseInt(scanner.nextLine());
+        for (int i = 0; i < cases; i++) {
+            int lower = scanner.nextInt();
+            int upper = scanner.nextInt();
+
+            out.println(squaresBetween(lower, upper));
+            scanner.nextLine();
+        }
+    }
+
+    private int squaresBetween(int lower, int upper) {
+        int squares = 0;
+
+        int lowerBound = (int)floor(sqrt(lower));
+        int upperBound = (int)ceil(sqrt(upper));
+
+        for (int i = lowerBound; i <= upperBound; i++) {
+            int square = i * i;
+            if (square >= lower && square <= upper) {
+                squares++;
+            }
+        }
+
+        return squares;
+
+    }
+}
---
+++ src/test/java/implementation/SherlockandSquaresTest.java
@@ -0,0 +1,24 @@
+package implementation;
+
+import common.Exercise;
+import common.InOutTest;
+import org.junit.Test;
+
+public class SherlockandSquaresTest extends InOutTest {
+    private Exercise exercise = new SherlockandSquares();
+
+    private static final String INPUT = "3\n" +
+            "3 9\n" +
+            "17 24\n" +
+            "100000000 1000000000\n";
+
+    private static final String OUTPUT = "2\n" +
+            "0\n" +
+            "21623\n";
+
+    @Test
+    public void shouldSolveExerciseForFirstTestInput() {
+        solve(INPUT, OUTPUT, exercise);
+    }
+
+}
