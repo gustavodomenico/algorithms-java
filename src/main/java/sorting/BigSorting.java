781b9723cb76292cd25c162aea50ba42889ffabf
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-09-18 11:54:08 -0300

    Add Big Sorting exercise.

---
+++ src/main/java/sorting/BigSorting.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import common.Exercise;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.in;
+import static java.lang.System.out;
+import static java.util.Arrays.sort;
+
+class BigSorting implements Exercise {
+    public void solve() {
+        Scanner scanner = new Scanner(in);
+
+        int numbers = parseInt(scanner.nextLine());
+
+        BigInteger[] array = new BigInteger[numbers];
+        for (int i = 0; i < numbers; i++) {
+            array[i] = scanner.nextBigInteger();
+        }
+
+        sort(array);
+
+        for (int i = 0; i < numbers; i++) {
+            out.println(array[i]);
+        }
+    }
+}
---
+++ src/test/java/sorting/BigSortingTest.java
@@ -0,0 +1,32 @@
+package sorting;
+
+import common.Exercise;
+import common.InOutTest;
+import org.junit.Test;
+
+public class BigSortingTest extends InOutTest {
+
+    private Exercise exercise = new BigSorting();
+
+    private static final String INPUT0 = "6\n" +
+            "31415926535897932384626433832795\n" +
+            "1\n" +
+            "3\n" +
+            "10\n" +
+            "3\n" +
+            "5\n";
+
+    private static final String OUTPUT0 = "1\n" +
+            "3\n" +
+            "3\n" +
+            "5\n" +
+            "10\n" +
+            "31415926535897932384626433832795\n";
+
+    @Test
+    public void shouldSolveExerciseForFirstTestInput() {
+        solve(INPUT0, OUTPUT0, exercise);
+    }
+
+
+}
