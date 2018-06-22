b282bc6700b03bfb4f25c01fa9d92d643a804f43
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-06-21 23:22:02 -0700

    Add minute to win it exercise.

---
+++ src/main/java/week38/MinuteToWinIt.java
@@ -0,0 +1,42 @@
+package week38;
+
+// https://www.hackerrank.com/contests/w38/challenges/minute-to-win-it
+
+// a[i] - a[i -1] = k
+// a[i] = lk + a[i -1]
+// a[i -1] = a[i] - k
+class MinuteToWinIt {
+    int minuteToWinIt(int[] a, int k) {
+        return Math.min(increasing(a.clone(), k), decreasing(a.clone(), k));
+    }
+
+    private int increasing(int[] a, int k) {
+        int moves = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            int curr = a[i] - a[i - 1];
+            if (curr != k) {
+                // do again
+                a[i] = a[i - 1] + k;
+                moves++;
+            }
+        }
+
+        return moves;
+    }
+
+    private int decreasing(int[] a, int k) {
+        int moves = 0;
+
+        for (int i = 0; i < a.length - 1; i++) {
+            int curr = a[i + 1] - a[i];
+            if (curr != k) {
+                // do again
+                a[i] = a[i + 1] - k;
+                moves++;
+            }
+        }
+
+        return moves;
+    }
+}
---
+++ src/test/java/week38/MinuteToWinItTest.java
@@ -0,0 +1,41 @@
+package week38;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MinuteToWinItTest {
+    private MinuteToWinIt exercise = new MinuteToWinIt();
+
+    @Test
+    public void shouldSolveExerciseForFirstInput() {
+        assertThat(exercise.minuteToWinIt(new int[] { 1, 2, 5, 7, 9, 85}, 2 ), is(2));
+    }
+
+    @Test
+    public void shouldSolveExerciseForSecondInput() {
+        assertThat(exercise.minuteToWinIt(new int[] { 4, 2, 0, -5, -6}, -2 ), is(2));
+    }
+
+    @Test
+    public void shouldSolveExerciseForThirdInput() {
+        assertThat(exercise.minuteToWinIt(new int[] { 0, 0, 0, 0, 0}, 0 ), is(0));
+    }
+
+    @Test
+    public void shouldSolveExerciseForFourthInput() {
+        assertThat(exercise.minuteToWinIt(new int[] { 1, 1, 1, 1, 1}, 0 ), is(0));
+    }
+
+    @Test
+    public void shouldSolveExerciseForFifthInput() {
+        assertThat(exercise.minuteToWinIt(new int[] { -4, -3, -1, 1, 3, 5, 7, 9}, 2 ), is(1));
+    }
+
+    @Test
+    public void shouldSolveExerciseForSixthInput() {
+        assertThat(exercise.minuteToWinIt(new int[] { 1, 3, -5, -6, 9, -9, 13}, 2 ), is(3));
+    }
+
+}
