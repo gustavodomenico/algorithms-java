9983070d18ab2bc7f1403bc0866f37f7de268998
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-07-01 15:11:36 -0700

    Add instructions.

--- src/main/java/interview/kit/arrays/TwoDimensionArray.java
+++ src/main/java/interview/kit/arrays/TwoDimensionArray.java
@@ -7,8 +7,16 @@ class TwoDimensionArray {
     int hourglassSum(int[][] arr) {
         int sum = Integer.MIN_VALUE;
 
+        // We just go row - 2 and col - 2...
         for (int i = 0; i < arr.length - 2; i++) {
             for (int j = 0; j < arr[i].length - 2; j++) {
+
+                // Basically get the hourglass positions like the problem instruct
+                // a b c
+                //   d
+                // e f g
+                // Note: pos + 0 is just for clarification
+
                 int a = arr[i][j + 0];
                 int b = arr[i][j + 1];
                 int c = arr[i][j + 2];
@@ -19,6 +27,7 @@ class TwoDimensionArray {
                 int f = arr[i + 2][j + 1];
                 int g = arr[i + 2][j + 2];
 
+                // Max between the current sum and the maximum already saved
                 sum = Math.max(sum, a + b + c + d + e + f + g);
             }
         }
