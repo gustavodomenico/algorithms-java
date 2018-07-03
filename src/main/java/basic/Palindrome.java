b8122c4de9953cb0c7561b34b523a38dcc22b167
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-07-02 21:04:09 -0700

    Add instrunctions.

--- src/main/java/basic/Palindrome.java
+++ src/main/java/basic/Palindrome.java
@@ -2,6 +2,7 @@ package basic;
 
 public class Palindrome {
     boolean isPalindrome(String s1) {
+        // Just test the N/2 first chars with the latest N/2 chars...
         for (int i = 0; i < s1.length() / 2; i++) {
             if (s1.charAt(i) != s1.charAt(s1.length() - i - 1)) {
                 return false;
